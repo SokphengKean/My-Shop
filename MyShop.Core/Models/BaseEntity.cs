fe2b76545f755396e6508791cd88a02efacd1384
Sokpheng <43340923+SokphengKean@users.noreply.github.com>
2021-03-01 15:27:27 +0700

    Implemented Generic Repository
    Create new Generic repository and fix _layout.cshtml bugs

---
+++ MyShop.Core/Models/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+	public abstract class BaseEntity
+	{
+		public BaseEntity()
+		{
+			this.Id = Guid.NewGuid().ToString();
+			this.CreateAt = DateTime.Now;
+		}
+
+		public string Id { get; set; }
+		public DateTimeOffset CreateAt { get; set; }
+
+	}
+}
---
+++ MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.InMemory
+{
+	public class InMemoryRepository<T> where T : BaseEntity
+	{
+		ObjectCache cache = MemoryCache.Default;
+		List<T> items;
+		string className;
+		public InMemoryRepository()
+		{
+			className = typeof(T).Name;
+			items = cache[className] as List<T>;
+			if (items == null)
+				items = new List<T>();
+		}
+
+		public void Commit()
+		{
+			cache[className] = items;
+		}
+
+		public void Insert(T t)
+		{
+			items.Add(t);
+		}
+
+		public void Update(T t)
+		{
+			var tUpdate = items.First(i => i.Id == t.Id);
+			if (tUpdate != null)
+				tUpdate = t;
+			else
+				throw new Exception(className + " not found.");
+		}
+
+		public void Delete(T t)
+		{
+			var tDelete = items.First(i => i.Id == t.Id);
+			if (tDelete != null)
+				items.Remove(tDelete);
+			else
+				throw new Exception(className + " not found.");
+		}
+
+		public T Find(string Id)
+		{
+			var tFind = items.First(i => i.Id == Id);
+			if (tFind != null)
+				return tFind;
+			else
+				throw new Exception(className + " not found.");
+		}
+
+		public IQueryable<T> Collection()
+		{
+			return items.AsQueryable();
+		}
+	}
+}
