0cbc7712b95a606e37678b91e195a73b47472305
Sokpheng <43340923+SokphengKean@users.noreply.github.com>
2021-03-03 17:13:18 +0700

    Added homepage product listing

--- MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop.WebUI/Controllers/HomeController.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyShop.DataAccess.SQL;
+using MyShop.Core.Models;
+using MyShop.Core.Contracts;
 
 namespace MyShop.WebUI.Controllers
 {
 	public class HomeController : Controller
 	{
+		IRepository<ProductCategory> category;
+		IRepository<Product> context;
+
+		public HomeController(IRepository<Product> context, IRepository<ProductCategory> category)
+		{
+			this.context = context;
+			this.category = category;
+		}
+
 		public ActionResult Index()
 		{
-			return View();
+			List<Product> products = context.Collection().ToList();
+			return View(products);
+		}
+
+		public ActionResult Detail(string Id)
+		{
+			var product = context.Find(Id);
+			if (product == null)
+				return HttpNotFound();
+			return View(product);
 		}
 
 		public ActionResult About()
